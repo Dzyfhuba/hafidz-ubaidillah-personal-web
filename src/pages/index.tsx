51d764cd2a4252af5d853fc02a6e3113fca1a093
Dzyfhuba <hafidz21ub@gmail.com>
2023-08-27 00:08:55 +0700

    goapi test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,34 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 
+
 const IndexPage: React.FC<PageProps> = () => {
+  const [data, setData] = React.useState()
+  const getData = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("X-API-KEY", "YLBYh9yKQgBui29zQH9gKJhOOFXUhZ");
+
+    fetch("https://api.goapi.id/v1/stock/idx/prices?symbols=AALI", {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    })
+      .then(async response => {
+        const data = await response.json()
+        setData(data.data)
+      })
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+  React.useEffect(() => {
+    getData()
+  }, [])
   return (
     <main>
       check
+      {
+        JSON.stringify(data)
+      }
     </main>
   )
 }
