55cc3c9ffb5cf8d41548c26414c6eb6f348e6bee
Dzyfhuba <hafidz21ub@gmail.com>
2023-10-06 12:29:56 +0700

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@ import styles from './footer.module.css'
 import supabase from '@/helpers/supabase'
 import moment from 'moment'
 import { Locale } from '@/i18n-config'
+import { getDictionary } from '@/get-dictionary'
 
 type Props = {
   lang: Locale
@@ -10,10 +11,11 @@ type Props = {
 
 const Footer = async (props: Props) => {
   const { data: setting } = await supabase.from('setting').select('*').single()
+  const t = await getDictionary(props.lang)
   return (
     <footer className={styles.footer}>
       <span>
-        Copyright &copy; 2023 <Link href='/' className={styles.link}>Hafidz Ubaidillah</Link>
+        {t.copyright} &copy; 2023 <Link href='/' className={styles.link}>Hafidz Ubaidillah</Link>
       </span>
       <span>
         Update: {moment(setting?.updated_at).format('DD MMMM YYYY')}
