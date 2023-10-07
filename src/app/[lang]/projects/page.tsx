73fb1f97983552bbc4b3625673139226662c6073
Dzyfhuba <hafidz21ub@gmail.com>
2023-10-07 22:46:08 +0700

    add project

--- src/app/[lang]/projects/page.tsx
+++ src/app/[lang]/projects/page.tsx
@@ -1,9 +1,34 @@
+import supabase from '@/helpers/supabase'
 import styles from './page.module.css'
 
 const Projects = async () => {
+  const { data } = await supabase.from('projects').select('*, project_platforms(platforms(id, platform)), project_tech_stacks(tech_stacks(text, icon))')
+
+
   return (
     <main className={styles.main}>
-      In progress...
+      {
+        data?.map((project) => (
+          <article key={project.id}>
+            <h2>{project.name}</h2>
+            <div>
+              {
+                project.project_platforms.map((platform) => (
+                  <span key={platform.platforms?.id}>{platform.platforms?.platform}</span>
+                ))
+              }
+            </div>
+            <div>
+              {
+                project.project_tech_stacks.map((tech_stack) => (
+                  <span key={tech_stack.tech_stacks?.id}>{tech_stack.tech_stacks?.text}</span>
+                ))
+              }
+            </div>
+          </article>
+        ))
+      }
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   )
 }
